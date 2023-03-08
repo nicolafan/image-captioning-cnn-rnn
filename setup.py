4bd388ab64a09b331a277df806585b6f6e46263e
nicolafan <48762613+nicolafan@users.noreply.github.com>
2023-03-08 16:35:47 +0100

    Load cookiecutter data science template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tensorflow/Keras implementation of an image captioning neural network, using CNN and RNN',
+    author='Nicola Fanelli',
+    license='MIT',
+)
