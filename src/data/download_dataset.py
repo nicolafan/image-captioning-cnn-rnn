c32c92b428d7d3921d7dd973f873bd2fb0229c94
nicolafan <48762613+nicolafan@users.noreply.github.com>
2023-03-08 17:22:38 +0100

    Blackify

--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -8,27 +8,24 @@ import kaggle
 
 @click.command()
 def main():
-    """ Download script for downloading the dataset (Flickr8k) from Kaggle
-    """
+    """Download script for downloading the dataset (Flickr8k) from Kaggle"""
     logger = logging.getLogger(__name__)
-    logger.info('downloading Flickr8k dataset from kaggle, please wait until new log message')
+    logger.info(
+        "downloading Flickr8k dataset from kaggle, please wait until new log message"
+    )
 
     project_dir = Path(__file__).resolve().parents[2]
     raw_data_dir = project_dir / "data" / "raw"
 
     kaggle.api.dataset_download_files(
-        "adityajn105/flickr8k",
-        path=raw_data_dir,
-        force=True,
-        quiet=True,
-        unzip=True
+        "adityajn105/flickr8k", path=raw_data_dir, force=True, quiet=True, unzip=True
     )
 
-    logger.info('download finished')
+    logger.info("download finished")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # find .env automagically by walking up directories until it's found, then
