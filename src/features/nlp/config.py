ada375630e07c6c70b132a3004a823a3223d54ac
nicolafan <48762613+nicolafan@users.noreply.github.com>
2023-03-10 21:01:40 +0100

    Add a base configuration for the tokenizer

---
+++ src/features/nlp/config.py
@@ -0,0 +1,11 @@
+class TokenizerConfig:
+    """A configuration for the untrained tokenizer
+
+    This configuration works as a single point for difining
+    the tokenizer config, and it will be loaded when instantiating
+    a tokenizer using `from_config`.
+    """
+    MAX_LEN = -1
+    VOCAB_SIZE = 8000
+    OOV = False
+    PAD_SEQUENCES = True
